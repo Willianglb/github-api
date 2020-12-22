e44e0179b248925f608d64eba155f07b9b221c9d
Willian Gabriel <48480780+Willianglb@users.noreply.github.com>
2020-12-22 13:29:49 -0300

    Update Repos.js

--- src/Repos.js
+++ src/Repos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { List, Icon} from "semantic-ui-react";
-import "../style.css";
+import "./style.css";
 
 const Repos = (props) => {
   const { listaRepos } = props;
